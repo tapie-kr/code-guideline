f1d928fe84256db11d5c549e8f4b11f3b9f5987e
cottons-kr <taeyeong.kim@algorix.io>
2025-01-29 11:54:37 +0900

    🔧 bump version to 0.0.4; update build script to clean dist folder; add rimraf as a dev dependency; adjust tsup configuration for target and output

--- tsup.config.mjs
+++ tsup.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'tsup';
+import { peerDependencies, dependencies } from './package.json';
 
 export default defineConfig({
   entry: ['eslint.config.mjs', 'prettier.config.mjs'],
   outDir: 'dist',
   format: ['esm', 'cjs'],
-  minify: true,
+  target: 'node20',
+  minify: false,
   outExtension: ({ format }) => (format === 'esm' ? { js: '.mjs' } : { js: '.cjs' }),
+  dts: true,
+  external: [...Object.keys(peerDependencies || {}), ...Object.keys(dependencies || {})],
 });
